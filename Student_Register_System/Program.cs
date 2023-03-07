0c48f7e8184a041ef3b8c2c71e6d150b89a96678
Jamie Miles <jamie.miles@weston.ac.uk>
2023-03-07 18:57:45 +0000

    Created basic structure for the menu system

--- Student_Register_System/Program.cs
+++ Student_Register_System/Program.cs
@@ -2,10 +2,31 @@
 {
     internal class Program
     {
+        
+        static int RunMenu(string[] menuItems)
+        {
+
+            Console.WriteLine(menuItems[0] + "\n");
+
+            for (int i = 1; i < menuItems.Length; i++)
+            {
+
+                Console.WriteLine(i + ": " + menuItems[i]);
+
+            }
+
+            Console.WriteLine("Please enter your selection from the list provided: \n");
+            int menuChoice = Convert.ToInt16(Console.ReadLine());
+
+            return menuChoice;
+
+        }
+        
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Hello, World!");
+            string[] menuItems = { "-----------Menu-----------", "Register Student", "Assign Student to Class", "Take Register", "Close"};
+            int menuChoice = RunMenu(menuItems);
 
         }
     }
