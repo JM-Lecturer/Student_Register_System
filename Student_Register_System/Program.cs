25f6ae43efcf8cfeca2da506b70433d22edf742e
Jamie Miles <jamie.miles@weston.ac.uk>
2023-03-07 19:07:14 +0000

    Added loop to the initial program menu

--- Student_Register_System/Program.cs
+++ Student_Register_System/Program.cs
@@ -15,8 +15,9 @@
 
             }
 
-            Console.WriteLine("Please enter your selection from the list provided: \n");
+            Console.WriteLine("\nPlease enter your selection from the list provided: \n");
             int menuChoice = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
 
             return menuChoice;
 
@@ -24,9 +25,44 @@
         
         static void Main(string[] args)
         {
+
+            int menuChoice = 0;
+
+            do
+            {
+
+                string[] menuItems = { "-----------Menu-----------", "Register Student", "Assign Student to Class", "Take Register", "Close" };
+                menuChoice = RunMenu(menuItems);
+
+                switch (menuChoice)
+                {
+
+                    case 1:
+
+                        Console.WriteLine("Register Students coming soon...");
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Assign Student to Class coming soon...");
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Take Register coming soon...");
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("Good Bye\n[Press Enter to Close]");
+                        Console.ReadLine();
+                        break;
+
+                }
+
+
+            } while (menuChoice != 4);
             
-            string[] menuItems = { "-----------Menu-----------", "Register Student", "Assign Student to Class", "Take Register", "Close"};
-            int menuChoice = RunMenu(menuItems);
 
         }
     }
